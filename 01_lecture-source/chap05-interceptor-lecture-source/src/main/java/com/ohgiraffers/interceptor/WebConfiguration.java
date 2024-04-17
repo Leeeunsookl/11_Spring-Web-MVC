85f7ad7f875e91a0333171fa9c761aab66c651d4
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-17 15:32:31 +0900

    [commit]

--- 01_lecture-source/chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/WebConfiguration.java
+++ 01_lecture-source/chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/WebConfiguration.java
@@ -15,6 +15,15 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(stopWatchInterceptor)
-
+                /* 어떤 url 요청에 따른 수행을 할건지 */
+                .addPathPatterns("/*")
+                /* 어떤 url 요청은 제외를 할건지
+                *   static 하위의 정적 리소스는 인터셉터가 적용되지 않도록 한다.
+                * */
+                .excludePathPatterns("/css/**")
+                .excludePathPatterns("/images/**")
+                .excludePathPatterns("/js/**")
+                /* /error 로 포워딩 되는 경로도 제외해주어야 한다. */
+                .excludePathPatterns("/error");
     }
 }
