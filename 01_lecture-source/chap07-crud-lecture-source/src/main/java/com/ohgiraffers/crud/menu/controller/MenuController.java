dd9bc3eeced99761284f6950dbbfae8d4b8484bd
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-19 15:00:14 +0900

    [commit]

--- 01_lecture-source/chap07-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
+++ 01_lecture-source/chap07-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
@@ -3,7 +3,10 @@ package com.ohgiraffers.crud.menu.controller;
 import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
 import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
 import com.ohgiraffers.crud.menu.model.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +16,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
+
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
 
+    private static final Logger logger = LogManager.getLogger(MenuController.class);
+
     private final MenuService menuService;
+    private final MessageSource messageSource;
 
     @Autowired
-    public MenuController(MenuService menuService) {
+    public MenuController(MenuService menuService, MessageSource messageSource) {
         this.menuService = menuService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/list")
@@ -46,9 +55,13 @@ public class MenuController {
     }
 
     @PostMapping("regist")
-    public String registMenu(MenuDTO newMenu, RedirectAttributes rttr) {
+    public String registMenu(MenuDTO newMenu, RedirectAttributes rttr, Locale locale) {
 
         menuService.registNewMenu(newMenu);
+        /* locale : 지역(나라) 에 대한 정보 다국어 처리와 관련 된 정보 */
+        logger.info("Locale : {}", locale);
+        rttr.addFlashAttribute("successMessage", messageSource.getMessage("registMenu", new Object[]{newMenu.getName()}, locale));
 
+        return "redirect:/menu/list";
     }
 }
